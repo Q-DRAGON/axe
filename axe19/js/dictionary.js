4ca1d90a5c4f5810b21afe0bdd67f73c64e3620c
Daisy <njtuwolf@126.com>
2017-11-15 16:50:44 +0800

    19初始化

---
+++ axe19/js/dictionary.js
@@ -0,0 +1,53 @@
+const d = {
+    0b0000000100000000: function(i){
+        if (i == undefined) {
+            return x
+        }else {
+            x = i
+        }
+    },
+    0b0000001000000000: function(i){
+        if (i == undefined) {
+            return y
+        }else {
+            y = i
+        }
+    },
+    0b0000001100000000: function(i){
+        if (i == undefined) {
+            return z
+        }else {
+            z = i
+        }
+    },
+    // 用于存储比较指令的结果 0 表示小于, 1 表示相等, 2 表示大于
+    0b0000010000000000: function(i){
+        if (i == undefined) {
+            return c1
+        }else {
+            c1 = i
+        }
+    },
+    // 用于存储当前寄存器暂存状态的内存地址
+    0b0000010100000000: function(i){
+        if (i == undefined) {
+            return f
+        }else {
+            f = i
+        }
+    },
+}
+
+const func_d = {
+    0b0000000000000000: set,
+    0b0000000100000000: load,
+    0b0000001000000000: add,
+    0b0000001100000000: save,
+    0b0000011100000000: save_from_register,
+    //  比较指令, 比较的是 x 和 y 的大小, 这个结果存在寄存器 c1 里面
+    0b0000010000000000: compare,
+    // 设置 pc 寄存器的值
+    0b0000010100000000: jump,
+    0b0000011000000000: jump_when_less,
+    0b1111111111111111: stop,
+}
---
+++ axe19/js/vm.js
@@ -0,0 +1,34 @@
+var pc = 0
+var x, y, z, f
+var c1 = -1
+
+const run = function(memory) {
+    // 这是一个虚拟机程序
+    // run 函数将 memory 数组视为内存，可以执行这个内存
+    log('old memory', memory)
+    memory = enhance_memory(memory, 65535)
+    while (pc < memory.length) {
+        func_d[memory[pc]](memory)
+    }
+    colorscreen(memory)
+    log('new memory', memory)
+}
+
+const colorscreen = function(memory){
+    let list = memory.slice(-10000,)
+    let canvas = _e('#id-canvas')
+    let context = canvas.getContext('2d')
+    let pixels = context.getImageData(0, 0, 32, 16)
+    let data = pixels.data
+    for (var i = 0; i < 512; i += 4) {
+        r = trans_to_RGBA(list[i / 4], 0, 4)
+        g = trans_to_RGBA(list[i / 4], 4, 8)
+        b = trans_to_RGBA(list[i / 4], 8, 12)
+        a = trans_to_RGBA(list[i / 4], 12, 16)
+        data[i] = r
+        data[i + 1] = g
+        data[i + 2] = b
+        data[i + 3] = a
+    }
+    context.putImageData(pixels, 0, 0)
+}
