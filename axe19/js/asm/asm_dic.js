78a5b509c654fb01dcdbf6069d51159b1561cb83
Daisy <daisy@DaisydeMacBook-Air.local>
2017-11-15 23:29:30 +0800

    asm_code写了一点

--- axe19/js/asm/asm_dic.js
+++ axe19/js/asm/asm_dic.js
@@ -16,9 +16,30 @@ const asm_d = {
     'stop': 0b1111111111111111,
 }
 
+// const asm_code = `
+// set y 55536                ; 左上角第一个像素
+// set z 404                  ; 用于斜方向设置像素，每两排设置一个
+// set x 61455                ; 红色
+// save_from_register x y     ; 设置像素点
+// add y z y
+// save_from_register x y     ; 设置像素点
+// add y z y
+// save_from_register x y     ; 设置像素点
+// add y z y
+// save_from_register x y     ; 设置像素点
+// add y z y
+// save_from_register x y     ; 设置像素点
+// stop                       ; 停止
+// `
+
 const asm_code = `
-set y 55536                ; 左上角第一个像素
-set z 404                  ; 用于斜方向设置像素，每两排设置一个
+jump @1024
+0b0000000000100000  ;.
+0b0000000000000000
+0b1000000001100000  ;,
+0b0000000000000000
+0b0101110011010110  ;$
+0b0111010000000000
 set x 61455                ; 红色
 save_from_register x y     ; 设置像素点
 add y z y
