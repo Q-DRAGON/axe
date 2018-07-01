8f8eec026252ba6960092386dc2ad6edee94296c
Daisy <njtuwolf@126.com>
2018-07-01 13:39:28 +0800

    srfa7 init

---
+++ srfa/sraf7-graph1/7-graph1.py
@@ -0,0 +1,34 @@
+本次作业周日晚上 22:00 回收
+
+本次作业目录结构
+srfa/07_graph1/graph1.py
+srfa/07_graph1/test_graph1.py
+srfa/07_graph1/watcher.py
+
+
+
+本次作业是 graph 相关的题目
+本次图的示例图在压缩包内 graph1.png
+
+
+你需要实现以下 2 个功能
+你需要自行实现建图
+用邻接表表示图，使用内置 list
+自行补充 Graph 需要的其他信息
+
+
+1， 给定的是左图（无向图），返回 value 元素和图的起点的距离
+    如果 value 不在图中，返回 -1
+distance(graph, value)
+
+
+2， 给定的是右图（有向无环图），返回图的拓扑排序，使用深度优先遍历
+拓扑排序是从偏序得到全序的操作，偏序的意思比如图中的 1 3，他们之间不能保证顺序，因为无法比较。
+topsort(graph)
+
+
+
+class Graph:
+    def __init__():
+        self.start = None
+
